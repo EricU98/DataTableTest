406154585606ea719a4cdbe4abcb051be7e4deb4
EricU98 <eric.uteg@web.de>
2022-07-13 15:27:39 +0200

    Projektdateien hinzufügen.

---
+++ DataTableTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+
+DataTable dataTable = new DataTable();
+
+dataTable.Columns.Add("Nummer");
+dataTable.Columns.Add("Bezeichnung");
+
+for (int i = 0; i < 10; i++)
+{
+    DataRow Row = dataTable.NewRow();
+    Row["Nummer"] = i;
+    Row["Bezeichnung"] = "Hallo Welt - " + i;
+    dataTable.Rows.Add(Row);
+}
+
+
+DataTable Table = new DataTable();
+Table.Columns.Add("Combined");
+
+
+foreach (DataRow row in dataTable.Rows)
+{
+    DataRow TableRow = Table.NewRow();
+    TableRow["Combined"] = row["Nummer"].ToString() + " - " + row["Bezeichnung"].ToString();
+    Table.Rows.Add(TableRow);
+}
+
+foreach (DataRow row in Table.Rows)
+{
+    Console.WriteLine(row["Combined"].ToString());
+}
+
+
+Console.ReadKey();
